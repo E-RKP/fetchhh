b2851feb221ccc3e8762c70cab8b58a8afea45a3
E-RKP <edobackup111@gmail.com>
2025-06-17 11:16:40 +0200

    okkk

--- data.js
+++ data.js
@@ -5,3 +5,20 @@ fetch("https://jsonplaceholder.typicode.com/posts").then(function (placeholder){
     console.log(placeholder[i])
   }
 })
+
+const uno = document.getElementById('uno')
+const due = document.getElementById('due')
+const tre = document.getElementById('tre')
+const quattro = document.getElementById('quattro')
+fetch("https://jsonplaceholder.typicode.com/users").then(function(placeholder){
+  return placeholder.json();
+}).then(function(placeholder){
+  for (i=0; i<placeholder.length;i++){
+
+    const li = document.createElement('li')
+    li.textContent = `Name: ${placeholder[i].name} |  Email: ${placeholder[i].email}`
+    document.body.appendChild(li)
+    
+  }
+ 
+})
